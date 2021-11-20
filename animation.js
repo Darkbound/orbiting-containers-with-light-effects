19e964decdfa4ae901abdfe10c6f246da2ab0327
Tod <vachev.t@gmail.com>
2021-11-21 01:06:26 +0200

    redone documentation

--- animation.js
+++ animation.js
@@ -1,35 +1,24 @@
 /**
- *
- * @param {{ mainContainerId: string,
- *           partnerLogosSize: number,
- *           partnerLogos: [{ src: string, lightColor: string }],
- *           ownLogo: { src: string, lightColor: string, size: number },
- *           circles: {
- *             outer: {
- *               color: string
- *               size: number
- *             },
- *             inner: {
- *               color: string
- *               size: number
- *             }
- *           },
- *           animations: {
- *             mainRotation: {
- *               speed: number
- *             },
- *             partnerLights: {
- *               speed: number,
- *               asynchronous: boolean
- *             },
- *             ownLight: {
- *               delay: number,
- *               speed: number,
- *               size: number
- *             }
- *           }
- * }} options animation options
- * @param options.mainContainerId the id of the main container in which the animation HTML will be inserted
+ * @param {Object} options options controlling the animation
+ * @param {string} options.mainContainerId the ID of the container in which the animation HTML will be inserted
+ * @param {number} options.partnerLogosSize the size of the orbiting logos, the number is treated as pixels size
+ * @param {Object} options.circles the object containing options of the background outer circles
+ * @param {Object} options.circles.outer the object containing options of the outer background circle
+ * @param {string} options.circles.outer.color the color of the outer circle
+ * @param {string} options.circles.outer.size the size of the outer circle
+ * @param {Object} options.circles.inner the object containing options of the inner background circle
+ * @param {string} options.circles.inner.color the color of the outer circle
+ * @param {string} options.circles.inner.size the size of the outer circle
+ * @param {Object} options.animations the object containing options for each animation
+ * @param {Object} options.animations.mainRotation the object containing options for the main rotation animation
+ * @param {number} options.animations.mainRotation.speed the speed of the rotation 10 units = 1s
+ * @param {Object} options.animations.partnerLights the object containing options for the orbiting logos lights animation
+ * @param {number} options.animations.partnerLights.speed the travel speed of the orbiting lights 10 units = 1s
+ * @param {boolean} options.animations.partnerLights.asynchronous the orbiting lights will travel to the center asynchronously
+ * @param {Object} options.animations.ownLight the object containing options for the central logo light animation
+ * @param {number} options.animations.ownLight.delay the delay of the light animation of the central logo
+ * @param {number} options.animations.ownLight.speed the duration of the central light 10 units = 1s
+ * @param {number} options.animations.ownLight.size the size of the central light, treated as px
  */
 function OrbitingLogosAnimation(options) {
   const { mainContainerId, circles, partnerLogosSize, partnerLogos, ownLogo, animations } = options;
