eee856d4dbde3dde4452424f0ff043255c06f540
Tod <vachev.t@gmail.com>
2021-11-20 23:23:28 +0200

    refactored into index.html

--- animation.js
+++ animation.js
@@ -1,33 +1,10 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const options = {
-    circles: {
-      outer: {
-        color: "7869cf", // must be HEX! Validate string length 6, characters 0-9A-F
-        size: 75,
-      },
-      inner: {
-        color: "69cfb6",
-        size: 75,
-      },
-    },
-    animations: {
-      mainRotation: {
-        speed: 500,
-      },
-      partnerLights: {
-        asynchronous: false,
-        speed: 50, //10 = 1s, //do some math and time it with the own logo animation
-      },
-      ownLight: {
-        delay: 20,
-        speed: 50,
-      },
-    },
-  };
+function OrbitingLogosAnimation(options) {
+  const mainContainer = document.querySelector(`#${options.mainContainerId}`);
+  const getMainContainerSmallestSize = () =>
+    mainContainer.clientWidth <= mainContainer.clientHeight ? mainContainer.clientWidth : mainContainer.clientHeight;
+  const mainContainerSize = getMainContainerSmallestSize();
 
   // TODO: Rename to orbiting planets/sun
-  // math about the sizes so that its responsive in any container
-  // what can be variable, and what must be predefined?
 
   class LogoMath {
     static RadiansToDegrees = (radians) => (radians * 180) / Math.PI;
@@ -56,23 +33,35 @@ window.addEventListener("DOMContentLoaded", () => {
 
   class CSS {
     static fullSize = { width: `100%`, height: `100%` };
-    static flexMiddle = { display: `flex`, alignItems: `center`, justifyContent: `center` };
+    static flexMiddle = {
+      display: `flex`,
+      alignItems: `center`,
+      justifyContent: `center`,
+    };
     static maxSize = (size) => ({ maxWidth: size, maxHeight: size });
     static squareSize = (size) => ({ width: size, height: size });
-    static absoluteCenter = (offset) => ({ position: `absolute`, left: offset, top: offset });
+    static absoluteCenter = (offset) => ({
+      position: `absolute`,
+      left: offset,
+      top: offset,
+    });
 
-    static mainWrapper = () => ({ ...CSS.fullSize, position: `relative` });
+    static mainWrapper = () => ({
+      ...CSS.fullSize,
+      ...CSS.maxSize(`${mainContainerSize}px`),
+      position: `relative`,
+    });
     static outerCircles = () => ({
-      ...CSS.squareSize(`530px`),
-      ...CSS.absoluteCenter(`35px`),
+      ...CSS.squareSize(`${mainContainerSize - options.partnerLogosSize}px`),
+      ...CSS.absoluteCenter(`${options.partnerLogosSize / 2}px`),
       borderRadius: `50%`,
       boxShadow: `0 0 0 ${options.circles.inner.size}px #${options.circles.inner.color} inset, 
                   0 0 0 ${options.circles.outer.size}px #${options.circles.outer.color}`,
     });
     static ownLogoContainer = () => ({
       ...CSS.fullSize,
-      ...CSS.maxSize(`90px`),
-      ...CSS.absoluteCenter(`calc(50% - 45px)`),
+      ...CSS.maxSize(`${options.ownLogo.size}px`),
+      ...CSS.absoluteCenter(`calc(50% - ${options.ownLogo.size / 2}px)`),
       zIndex: `10`,
     });
     static ownLogo = () => ({
@@ -82,7 +71,7 @@ window.addEventListener("DOMContentLoaded", () => {
     static ownLogoLight = () => ({
       ...CSS.fullSize,
       ...CSS.maxSize(`1px`),
-      ...CSS.absoluteCenter(`45px`),
+      ...CSS.absoluteCenter(`${options.ownLogo.size / 2}px`),
       borderRadius: `50%`,
       animation: `ownLogoAnimation ${LogoMath.convertSpeedToTime(options.animations.ownLight.speed)}s infinite`,
       animationDelay: `${LogoMath.convertSpeedToTime(options.animations.ownLight.delay)}s`,
@@ -91,26 +80,26 @@ window.addEventListener("DOMContentLoaded", () => {
     });
     static ownLogoImage = () => ({
       ...CSS.fullSize,
-      ...CSS.maxSize(`90px`),
+      ...CSS.maxSize(`${options.ownLogo.size}px`),
     });
     static partnerLogoContainer = () => ({
       ...CSS.flexMiddle,
       position: `absolute`,
-      top: `calc(50% - 32.5px)`,
-      width: `300px`,
-      height: `65px`,
+      top: `calc(50% - ${options.partnerLogosSize / 2}px)`,
+      width: `${mainContainerSize / 2}px`,
+      height: `${options.partnerLogosSize}px`,
       transformOrigin: `center right`,
     });
     static partnerLogo = () => ({
       ...CSS.fullSize,
       ...CSS.flexMiddle,
-      ...CSS.maxSize(`70px`),
+      ...CSS.maxSize(`${options.partnerLogosSize}px`),
       borderRadius: `50%`,
       border: `1px solid black`,
     });
     static partnerLogoImg = () => ({
       ...CSS.fullSize,
-      ...CSS.maxSize(`70px`),
+      ...CSS.maxSize(`${options.partnerLogosSize}px`),
     });
     static partnerLogoLine = () => ({
       width: `100%`,
@@ -129,12 +118,14 @@ window.addEventListener("DOMContentLoaded", () => {
       backgroundColor: `blue`,
       borderRadius: `50%`,
       position: `absolute`,
-      left: `35px`,
+      left: `${options.partnerLogosSize / 2}px`,
       animation: `partnerLightAnimation ${LogoMath.convertSpeedToTime(
         options.animations.partnerLights.speed
       )}s ease-in infinite`,
     });
-    static lightGlow = ({ color = "#FFFFFF", size = "20px" }) => ({ boxShadow: `0 0 20px ${size} ${color}` });
+    static lightGlow = ({ color = "#FFFFFF", size = "20px" }) => ({
+      boxShadow: `0 0 20px ${size} ${color}`,
+    });
     static rotate = (deg) => ({ transform: `rotate(${deg}deg)` });
     static counterRotatingAnimation = (initialAngle) => ``;
 
@@ -152,11 +143,15 @@ window.addEventListener("DOMContentLoaded", () => {
       partnerLight: `
       @keyframes partnerLightAnimation {
         0% {
-          left: 35px;
+          opacity: 0;
+          left: 35px;   
+        }
+        40% {
+          left: 35px;   
           opacity: 1;
         }
       
-        40% {
+        80% {
           left: 100%;
         }
       
@@ -171,7 +166,7 @@ window.addEventListener("DOMContentLoaded", () => {
           opacity: 0;
         }
       
-        40% {
+        70% {
           opacity: 1;
         }
 
@@ -202,212 +197,54 @@ window.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-  const ownLogo = {
-    src: "img/pancake.png",
-    lightColor: "#00FF00",
-  };
-
-  const logos = [
-    {
-      src: "img/binance.png",
-      lightColor: "#ffcc00",
-    },
-    {
-      src: "img/kucoin.png",
-      lightColor: "#6699ff",
-    },
-    {
-      src: "img/hit.png",
-      lightColor: "#cc3399",
-    },
-    {
-      src: "img/huobi.png",
-      lightColor: "#0066cc",
-    },
-    {
-      src: "img/kraken.png",
-      lightColor: "#00ff99",
-    },
-    {
-      src: "img/pancake.png",
-      lightColor: "#9900ff",
-    },
-    {
-      src: "img/binance.png",
-      lightColor: "#ffcc00",
-    },
-    {
-      src: "img/kucoin.png",
-      lightColor: "#6699ff",
-    },
-    {
-      src: "img/hit.png",
-      lightColor: "#cc3399",
-    },
-    {
-      src: "img/huobi.png",
-      lightColor: "#0066cc",
-    },
-    {
-      src: "img/kraken.png",
-      lightColor: "#00ff99",
-    },
-    {
-      src: "img/pancake.png",
-      lightColor: "#9900ff",
-    },
-    {
-      src: "img/binance.png",
-      lightColor: "#ffcc00",
-    },
-    {
-      src: "img/kucoin.png",
-      lightColor: "#6699ff",
-    },
-    {
-      src: "img/hit.png",
-      lightColor: "#cc3399",
-    },
-    {
-      src: "img/huobi.png",
-      lightColor: "#0066cc",
-    },
-    {
-      src: "img/kraken.png",
-      lightColor: "#00ff99",
-    },
-    {
-      src: "img/pancake.png",
-      lightColor: "#9900ff",
-    },
-    {
-      src: "img/binance.png",
-      lightColor: "#ffcc00",
-    },
-    {
-      src: "img/kucoin.png",
-      lightColor: "#6699ff",
-    },
-    {
-      src: "img/hit.png",
-      lightColor: "#cc3399",
-    },
-    {
-      src: "img/huobi.png",
-      lightColor: "#0066cc",
-    },
-    {
-      src: "img/kraken.png",
-      lightColor: "#00ff99",
-    },
-    {
-      src: "img/pancake.png",
-      lightColor: "#9900ff",
-    },
-    {
-      src: "img/binance.png",
-      lightColor: "#ffcc00",
-    },
-    {
-      src: "img/kucoin.png",
-      lightColor: "#6699ff",
-    },
-    {
-      src: "img/hit.png",
-      lightColor: "#cc3399",
-    },
-    {
-      src: "img/huobi.png",
-      lightColor: "#0066cc",
-    },
-    {
-      src: "img/kraken.png",
-      lightColor: "#00ff99",
-    },
-    {
-      src: "img/pancake.png",
-      lightColor: "#9900ff",
-    },
-  ];
-
   // HTML
-  const mainContainerId = "logoAnimationContainer";
-  const mainContainer = document.querySelector(`#${mainContainerId}`);
-
-  mainContainer.innerHTML += `<style>
+  const insertHTML = () => {
+    mainContainer.innerHTML += `<style>
                                 ${Object.keys(CSS.keyframes)
                                   .map((key) => CSS.keyframes[key])
                                   .join(" ")}
                               </style>`;
 
-  mainContainer.innerHTML += HTML.outerCircles();
-  mainContainer.innerHTML += HTML.logoAnimationWrapper();
+    mainContainer.innerHTML += HTML.logoAnimationWrapper();
 
-  Selectors.mainWrapper().innerHTML += HTML.ownLogo(ownLogo.src);
-  Selectors.mainWrapper().innerHTML += HTML.partnerLogosContainer();
+    Selectors.mainWrapper().innerHTML += HTML.outerCircles();
+    Selectors.mainWrapper().innerHTML += HTML.ownLogo(options.ownLogo.src);
+    Selectors.mainWrapper().innerHTML += HTML.partnerLogosContainer();
 
-  for (let i = 0; i < logos.length; i++) {
-    Selectors.partnerLogosContainer().innerHTML += HTML.partnerLogos(logos[i].src);
-  }
+    for (let i = 0; i < options.partnerLogos.length; i++) {
+      Selectors.partnerLogosContainer().innerHTML += HTML.partnerLogos(options.partnerLogos[i].src);
+    }
+  };
 
   // CSS
-  Object.keys(Selectors).forEach(
-    (selector) => selector !== "arrays" && Object.assign(Selectors[selector]().style, CSS[selector]())
-  );
+  const applyCSS = () => {
+    Object.keys(Selectors).forEach(
+      (selector) => selector !== "arrays" && Object.assign(Selectors[selector]().style, CSS[selector]())
+    );
 
-  Object.keys(Selectors.arrays).forEach((arraySelector) => {
-    for (let i = 0; i < Selectors.arrays[arraySelector]().length; i++) {
-      Object.assign(Selectors.arrays[arraySelector]()[i].style, CSS[arraySelector]());
-    }
-  });
-
-  Object.assign(Selectors.ownLogoLight().style, {
-    ...CSS.lightGlow({ color: ownLogo.lightColor, size: "50px" }),
-  });
-
-  for (let i = 0; i < logos.length; i++) {
-    let initialAngle = LogoMath.RadiansToDegrees(LogoMath.EquallySpacedCircleAngle(logos.length) * i);
-    Selectors.arrays.partnerLogoContainer()[i].style.transform = CSS.rotate(initialAngle).transform;
-
-    Object.assign(Selectors.arrays.partnerLogoLight()[i].style, {
-      ...CSS.lightGlow({ color: logos[i].lightColor }),
+    Object.keys(Selectors.arrays).forEach((arraySelector) => {
+      for (let i = 0; i < Selectors.arrays[arraySelector]().length; i++) {
+        Object.assign(Selectors.arrays[arraySelector]()[i].style, CSS[arraySelector]());
+      }
     });
-  }
-});
 
-//   const convertXYtoLeftTop = ({ x, y, totalWidth, totalHeight }) => {
-//     const originX = totalWidth / 2;
-//     const originY = totalHeight / 2;
+    Object.assign(Selectors.ownLogoLight().style, {
+      ...CSS.lightGlow({
+        color: options.ownLogo.lightColor,
+        size: `${options.animations.ownLight.size}px`,
+      }),
+    });
 
-//     console.log(originX);
+    for (let i = 0; i < options.partnerLogos.length; i++) {
+      let initialAngle = LogoMath.RadiansToDegrees(LogoMath.EquallySpacedCircleAngle(options.partnerLogos.length) * i);
+      Selectors.arrays.partnerLogoContainer()[i].style.transform = CSS.rotate(initialAngle).transform;
 
-//     const left = `${originX + x - 32.5}px`;
-//     const top = `${originY - y - 32.5}px`;
+      Object.assign(Selectors.arrays.partnerLogoLight()[i].style, {
+        ...CSS.lightGlow({ color: options.partnerLogos[i].lightColor }),
+      });
+    }
+  };
 
-//     return { left, top };
-//   };
-//   const setWidthAndHeightOfContainer = ({ container }) => ({
-//     totalWidth: container.clientWidth <= container.clientHeight ? container.clientWidth : container.clientHeight,
-//     totalHeight: container.clientWidth <= container.clientHeight ? container.clientWidth : container.clientHeight,
-//   });
-//   const pointOnCircleCoordinates = ({ radius, angle }) => ({
-//     x: radius * Math.cos(angle),
-//     y: radius * Math.sin(angle),
-//   });
-
-//   const mainContainer = document.querySelector("#logoAnimationContainer");
-//   const partnerLogoContainers = document.querySelectorAll(".partnerLogoContainer");
-
-//   const { totalWidth, totalHeight } = setWidthAndHeightOfContainer({ container: mainContainer });
-//   // Take the smallest of the two sides in case the container is not a square
-//   const radius = totalWidth / 2 - 32.5;
-//   const angle = (Math.PI * 2) / partnerLogoContainers.length;
-
-//   for (let i = 0; i < partnerLogoContainers.length; i++) {
-//     const { x, y } = pointOnCircleCoordinates({ radius, angle: angle * i });
-//     console.log({ x, y });
-//     const { left, top } = convertXYtoLeftTop({ x, y, totalWidth, totalHeight });
-
-//     partnerLogoContainers[i].style.left = left;
-//     partnerLogoContainers[i].style.top = top;
-//   }
+  insertHTML();
+  applyCSS();
+}
